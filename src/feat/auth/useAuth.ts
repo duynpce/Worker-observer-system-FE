071b2adfc3e5e0cf8c0978194ca4581cb48f2d6d
duynpce <a23.thvl03@gmail.com>
2026-05-12 10:08:19 +0700

    chore: rename authType  to auth.type for consistency

--- src/feat/auth/useAuth.ts
+++ src/feat/auth/useAuth.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query"
 import { login } from "./authService"
-import type { LoginRequest } from "./authType"
+import type { LoginRequest } from "./auth.type"
 
 export const useLogin = () => { useMutation({
   mutationFn: (request: LoginRequest) => login(request)
